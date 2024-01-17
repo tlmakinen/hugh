1ef5ca8ca7ff34e46c0f8f98bef1fe38eda4b1ff
tlmakinen <lucas.makinen@gmail.com>
2024-01-17 17:11:45 +0100

    working torch dataloader from h5

--- torch_unet/dataloader.py
+++ torch_unet/dataloader.py
@@ -1,127 +1,133 @@
-
 import h5py
 import helpers
 import numpy as np
 from pathlib import Path
 import torch
-from torch.utils import data
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.sampler import Sampler
+import os.path as osp
+import os
 
-class HDF5Dataset(data.Dataset):
-    """Represents an abstract HDF5 dataset.
-    
-    Input params:
-        file_path: Path to the folder containing the dataset (one or multiple HDF5 files).
-        recursive: If True, searches for h5 files in subdirectories.
-        load_data: If True, loads all the data immediately into RAM. Use this if
-            the dataset is fits into memory. Otherwise, leave this at false and 
-            the data will load lazily.
-        data_cache_size: Number of HDF5 files that can be cached in the cache (default=3).
-        transform: PyTorch transform to apply to every data instance (default=None).
-    """
-    def __init__(self, file_path, recursive, load_data, data_cache_size=3, transform=None):
-        super().__init__()
-        self.data_info = []
-        self.data_cache = {}
-        self.data_cache_size = data_cache_size
-        self.transform = transform
+class H5Dataset(Dataset):
+    def __init__(self, cosmo_h5_file_paths, 
+                 gal_h5_file_paths, 
+                 use_cache=False,
+                 loadcache=False,
+                 root="/data101/makinen/hirax_sims/dataloader/"):
+        
+        self.cosmo_h5_file_paths = cosmo_h5_file_paths
+        self.gal_h5_file_paths = gal_h5_file_paths
+        self.use_cache = use_cache
 
-        # Search for all h5 files
-        p = Path(file_path)
-        assert(p.is_dir())
-        if recursive:
-            files = sorted(p.glob('**/*.h5'))
+        self.cosmo_samples = []
+        self.gal_samples = []
+        
+        
+        # gather lists of cosmo and galaxy samples
+        for i,path in enumerate(cosmo_h5_file_paths):
+            # each index becomes the key
+            h5_file = h5py.File(path, 'r')
+            self.cosmo_samples.append({'file': h5_file, 'key': i})
+                        
+        for path in gal_h5_file_paths:
+            h5_file = h5py.File(path, 'r')
+            self.gal_samples.append({'file': h5_file})
+        
+        
+        # initialise a random index for the galaxy samples
+        self.gal_indices = torch.randperm(len(gal_h5_file_paths))
+        self.len_gal = len(gal_h5_file_paths)
+                
+        self.root = root
+        
+        # check to see if the root dir and x and y cache are there
+        # and load 
+        if loadcache:
+            if osp.exists(root + "gal_cache.pt"):
+                self.load_cache()
+
         else:
-            files = sorted(p.glob('*.h5'))
-        if len(files) < 1:
-            raise RuntimeError('No hdf5 datasets found')
+            self.gal_cache = []
+            self.cosmo_cache = []
+        
 
-        for h5dataset_fp in files:
-            self._add_data_infos(str(h5dataset_fp.resolve()), load_data)
+    # the length of the dataset is determined by the length of the cosmo map dataset
+    def __len__(self):
+        return len(self.cosmo_samples)
+
+    # this method returns (x,y) = (cosmo + galaxy, cosmo) training pairs
+    # here idx indexes the cosmology simulations (foregrounds will be random)
+    def __getitem__(self, idx):
+        
+        if not self.use_cache:
+            # choose the file to read in
+            cosmo_sample_info = self.cosmo_samples[idx]
+            cosmo_file = cosmo_sample_info['file']
             
-    def __getitem__(self, index):
-        # get data
-        x = self.get_data("data", index)
-        if self.transform:
-            x = self.transform(x)
-        else:
-            x = torch.from_numpy(x)
+            cosmo = torch.tensor(np.array(cosmo_file['/vis/']), dtype=torch.complex64)
 
-        # get label
-        y = self.get_data("label", index)
-        y = torch.from_numpy(y)
-        return (x, y)
+            # add random foregrounds
+            rand_idx = torch.randint(low=0, high=self.len_gal, size=()) #self.gal_indices[idx]
+            gal_sample_info = self.gal_samples[rand_idx]
+
+            gal_file = gal_sample_info['file']
+            gal = torch.tensor(np.array(gal_file['/vis/']), dtype=torch.complex64)
+
+            self.gal_cache.append(gal)
+            self.cosmo_cache.append(cosmo)
+            
+            # now get x and y
+            x = gal + cosmo
+            y = cosmo
+            
+            
+        else:
+            # randomise foreground and cosmology combinations from cache
+            rand_idx = torch.randint(low=0, high=len(self.gal_cache), size=())
+            gal = self.gal_cache[rand_idx] # random index from cache
+            y = self.cosmo_cache[idx] # index of cosmological simulation
+            x = gal + y
+            
+        return x, y
+    
+    def set_use_cache(self, use_cache):
+        if use_cache:
+            self.gal_cache = torch.stack(self.gal_cache)
+            self.cosmo_cache = torch.stack(self.cosmo_cache)
+        else:
+            self.gal_cache = []
+            self.cosmo_cache = []
+        self.use_cache = use_cache
+        
+    
+    def save_cache(self):
+        if not osp.exists(self.root):
+            os.makedirs(self.root)
+        torch.save(torch.stack(self.gal_cache), self.root + "gal_cache.pt")
+        torch.save(torch.stack(self.cosmo_cache), self.root + "cosmo_cache.pt")
+        
+    def load_cache(self):
+        self.gal_cache = torch.load(self.root + "gal_cache.pt")
+        self.cosmo_cache = torch.load(self.root + "cosmo_cache.pt")
+        self.use_cache = True
+    
+
+    def __del__(self):
+        for sample_info in self.cosmo_samples:
+            sample_info['file'].close()
+        
+        for sample_info in self.gal_samples:
+            sample_info['file'].close()
+
+class InfiniteSampler(Sampler):
+    def __init__(self, data_source):
+        self.num_samples = len(data_source)
+
+    def __iter__(self):
+        while True:
+            order = torch.randperm(self.num_samples)
+            for i in range(self.num_samples):
+                yield order[i]
 
     def __len__(self):
-        return len(self.get_data_infos('data'))
-    
-    def _add_data_infos(self, file_path, load_data):
-        with h5py.File(file_path) as h5_file:
-            # Walk through all groups, extracting datasets
-            for gname, group in h5_file.items():
-                for dname, ds in group.items():
-                    # if data is not loaded its cache index is -1
-                    idx = -1
-                    if load_data:
-                        # add data to the data cache
-                        idx = self._add_to_cache(ds.value, file_path)
-                    
-                    # type is derived from the name of the dataset; we expect the dataset
-                    # name to have a name such as 'data' or 'label' to identify its type
-                    # we also store the shape of the data in case we need it
-                    self.data_info.append({'file_path': file_path, 'type': dname, 'shape': ds.value.shape, 'cache_idx': idx})
-
-    def _load_data(self, file_path):
-        """Load data to the cache given the file
-        path and update the cache index in the
-        data_info structure.
-        """
-        with h5py.File(file_path) as h5_file:
-            for gname, group in h5_file.items():
-                for dname, ds in group.items():
-                    # add data to the data cache and retrieve
-                    # the cache index
-                    idx = self._add_to_cache(ds.value, file_path)
-
-                    # find the beginning index of the hdf5 file we are looking for
-                    file_idx = next(i for i,v in enumerate(self.data_info) if v['file_path'] == file_path)
-
-                    # the data info should have the same index since we loaded it in the same way
-                    self.data_info[file_idx + idx]['cache_idx'] = idx
-
-        # remove an element from data cache if size was exceeded
-        if len(self.data_cache) > self.data_cache_size:
-            # remove one item from the cache at random
-            removal_keys = list(self.data_cache)
-            removal_keys.remove(file_path)
-            self.data_cache.pop(removal_keys[0])
-            # remove invalid cache_idx
-            self.data_info = [{'file_path': di['file_path'], 'type': di['type'], 'shape': di['shape'], 'cache_idx': -1} if di['file_path'] == removal_keys[0] else di for di in self.data_info]
-
-    def _add_to_cache(self, data, file_path):
-        """Adds data to the cache and returns its index. There is one cache
-        list for every file_path, containing all datasets in that file.
-        """
-        if file_path not in self.data_cache:
-            self.data_cache[file_path] = [data]
-        else:
-            self.data_cache[file_path].append(data)
-        return len(self.data_cache[file_path]) - 1
-
-    def get_data_infos(self, type):
-        """Get data infos belonging to a certain type of data.
-        """
-        data_info_type = [di for di in self.data_info if di['type'] == type]
-        return data_info_type
-
-    def get_data(self, type, i):
-        """Call this function anytime you want to access a chunk of data from the
-            dataset. This will make sure that the data is loaded in case it is
-            not part of the data cache.
-        """
-        fp = self.get_data_infos(type)[i]['file_path']
-        if fp not in self.data_cache:
-            self._load_data(fp)
-        
-        # get new cache_idx assigned by _load_data_info
-        cache_idx = self.get_data_infos(type)[i]['cache_idx']
-        return self.data_cache[fp][cache_idx]
+        return 2**31
